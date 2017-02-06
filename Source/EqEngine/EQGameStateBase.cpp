8c6b6973bfbd7f816a3f59baef8b8b5645ee5498
Zino2201 <contactminecraftliop@gmail.com>
2017-02-06 23:42:16 +0100

    Player State

--- Source/EqEngine/EQGameStateBase.cpp
+++ Source/EqEngine/EQGameStateBase.cpp
@@ -3,6 +3,17 @@
 #include "EqEngine.h"
 #include "EQGameStateBase.h"
 
+AEQGameStateBase::AEQGameStateBase()
+{
+	CurrentPlayerState = 2; // 2 = Offline, 1 = InGame, 0 = Connected
+}
 
+int32 AEQGameStateBase::getCurrentPlayerState()
+{
+	return CurrentPlayerState;
+}
 
-
+void AEQGameStateBase::setCurrentPlayerState(int32 NewState)
+{
+	CurrentPlayerState = NewState;
+}
